51d4b4bbd3bb8a48452c66f9bccddfdb78f8eb3c
TejasBedarkar <tejasaidb11@gmail.com>
2026-03-09 13:31:44 +0530

    student view list tmep

--- student_project/students/views.py
+++ student_project/students/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render          #render HTML templates.
+from .models import Student                  #to query Student table
 
 # Create your views here.
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
